09106b3aa7b83b81095fbcd97ebcd19a5027c128
Brian Li <brian.li.fml@gmail.com>
2021-04-18 21:04:37 -0700

    this is automaticly generated by LIBGDX

---
+++ android/src/com/ppzl/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ppzl.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ppzl.game.MyPuzzleGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyPuzzleGame(), config);
+	}
+}
