e9041055d26ad5a8f69ff8816671f5c27bc7e0bd
Brian Li <brian.li.fml@gmail.com>
2021-04-21 20:39:13 -0700

    add drop game do raindrops next

--- core/src/com/ppzl/game/MyPuzzleGame.java
+++ core/src/com/ppzl/game/MyPuzzleGame.java
@@ -1,31 +1,79 @@
 package com.ppzl.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyPuzzleGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Rectangle bucket;
+	private Vector3 touchPos;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// load the images for the droplet and the bucket, 64x64 pixels each
+		FileHandle gg = Gdx.files.internal("droplet.png");
+		dropImage = new Texture(gg);
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// load the drop sound effect and the rain background "music"
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// start the playback of the background music immediately
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		float x = 800 / 2 - 64 / 2;
+		float y = 20;
+		float width = 64;
+		float height = 64;
+		bucket = new Rectangle(x, y, width, height);
+
+		touchPos = new Vector3();
+		// ... more to come ...
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
+
+		if(Gdx.input.isTouched()) {
+
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+			
+			if(bucket.x < 0) bucket.x = 0;
+			if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		dropImage.dispose();
 	}
 }
--- desktop/src/com/ppzl/game/desktop/DesktopLauncher.java
+++ desktop/src/com/ppzl/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.ppzl.game.MyPuzzleGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Picture Puzzle";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new MyPuzzleGame(), config);
 	}
 }
