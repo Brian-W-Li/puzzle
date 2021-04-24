23393a28451e73505d234b29fd9ac5f746e89897
Brian Li <brian.li.fml@gmail.com>
2021-04-23 19:45:04 -0700

    we finished the small one

--- core/src/com/ppzl/game/MyPuzzleGame.java
+++ core/src/com/ppzl/game/MyPuzzleGame.java
@@ -8,9 +8,14 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class MyPuzzleGame extends ApplicationAdapter {
 	private Texture dropImage;
@@ -21,6 +26,8 @@ public class MyPuzzleGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private Rectangle bucket;
 	private Vector3 touchPos;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -47,19 +54,36 @@ public class MyPuzzleGame extends ApplicationAdapter {
 		bucket = new Rectangle(x, y, width, height);
 
 		touchPos = new Vector3();
+
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
 		// ... more to come ...
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
+
 		camera.update();
 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
-
 		if(Gdx.input.isTouched()) {
 
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
@@ -75,5 +99,19 @@ public class MyPuzzleGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+
 	}
 }
